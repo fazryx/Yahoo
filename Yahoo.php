fb7fd2084b5625eb066c11145032271e2abce56e
fazryx <67833196+fazryx@users.noreply.github.com>
2020-07-05 21:49:17 +0700

    Add files via upload

---
+++ Yahoo.php
@@ -0,0 +1,51 @@
+<?php
+error_reporting(0);
+system("clear");
+$banner = "\e[33;1m                                                                                 
+FAZRY X 
+                                                                            
+
+    [--------------------------------------- ]                              
+[--------------> \033[1;32mYahoo Mail Valid\e[0;1m\e[33;1m <---------------]
+    [---------------------------------------]
+                                                                            
+ Coded By : FAZRY X || FAZRY X LIONA
+ Github   : https://github.com/fazryx\n\n\e[0;1m";
+sleep(2);
+echo $banner;
+sleep(2);
+echo "[\033[1;31m?\e[0;1m] List Mail : ";
+$list = trim(fgets(STDIN));
+echo "\n";
+$file = file_get_contents("$list");
+$data = explode("\n", str_replace("\r", "", $file));
+$x = 0;
+for ($a = 0; $a < count($data); $a++) {
+    $email   = $data[$a];
+    $x++;
+     
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://api.nabill.me/yahoo.php?email='.$email);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, true);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    
+    $headers   = array();
+    $headers[] = 'Connection: Keep-Alive';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    
+    $result = curl_exec($ch);
+    $hasil = json_decode($result);
+    curl_close($ch);
+	$status = $hasil->status;
+	if($status == 'live'){
+		echo "$email | \033[1;32mLive\e[0;1m\n";
+		$x = fopen("Yahoo_LIVE.txt", "a+");
+		fwrite($x, $email."\r\n");
+	}else{
+		echo "$email | \e[1;91mDie\e[0;1m\n";
+		$x = fopen("Yahoo_DIE.txt", "a+");
+		fwrite($x, $email."\r\n");
+    }
+}
+?>
